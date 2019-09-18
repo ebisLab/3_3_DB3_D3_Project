7d32379eda4d43509efd27de91f9aee52efd0e20
ebisLab <ebaiden@techie.com>
2019-09-18 12:25:22 -0400

    token sent

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken')
 
 const Users = require('../users/users-model.js');
 
@@ -25,9 +26,9 @@ router.post('/login', (req, res) => {
     .first()
     .then(user => {
       if (user && bcrypt.compareSync(password, user.password)) {
-        res.status(200).json({
-          message: `Welcome ${user.username}!`,
-        });
+        //need a way to get that token
+        const token = generateToken(user);
+        res.status(200).json({ token });
       } else {
         res.status(401).json({ message: 'Invalid Credentials' });
       }
@@ -36,5 +37,17 @@ router.post('/login', (req, res) => {
       res.status(500).json(error);
     });
 });
+
+function generateToken(user) {
+  const payload = {
+    username: user.username,
+  }
+  const secret = 'keept it secrt, keep it safe! '
+  const options = {
+    //expiration of the token
+    expiresIn: '1d', //one day
+  }
+  return jwt.sign(payload, secret, options);
+}
 
 module.exports = router;
